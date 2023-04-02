820291dba2c0a4e7b0b1c2aa420a0f091dbc4b63
faib <faib920@126.com>
2023-04-03 00:14:33 +0800

    神通数据库语法单元测试

---
+++ src/tests/Fireasy.Data.Tests/SyntaxTest/SyntaxTests.ShenTong.cs
@@ -0,0 +1,11 @@
+﻿namespace Fireasy.Data.Tests.SyntaxTest
+{
+    [TestClass]
+    [TestCategory("SyntaxTests")]
+    public class SyntaxTests_ShenTong : SyntaxTests<ShenTongProvider>
+    {
+        protected override string ConnectionString => Constants.ShenTong_ConnectionString;
+
+        protected override string InstanceName => Constants.ShenTong_InstanceName;
+    }
+}
